1eb865f47cc2f1b80211953ff2553dc97232ca76
Dale Schumacher <dalnefre@yahoo.com>
2013-12-26 10:45:38 -0600

    Disable noisy console output

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ marshal.domain = function domain(name, sponsor) {
         var remote;
         for (remote in tokenMap) {
             if (tokenMap[remote] === local) {
-                console.log('localToRemote(found):', name, remote);
+//                console.log('localToRemote(found):', name, remote);
                 return remote;
             }
         }
         /* not found, create a new entry */
         remote = encodeToken(generateToken());
         tokenMap[remote] = local;
-        console.log('localToRemote(created):', name, remote);
+//        console.log('localToRemote(created):', name, remote);
         return remote;
     };
     var generateToken = function generateToken() {
@@ -69,7 +69,7 @@ marshal.domain = function domain(name, sponsor) {
         }
     };
     var encodeToken = function encodeToken(value) {
-        console.log('encodeToken:', name, value);
+//        console.log('encodeToken:', name, value);
         return "=" + name + "#" + value;
     };
 
@@ -79,11 +79,11 @@ marshal.domain = function domain(name, sponsor) {
             local = proxy(remote);
             tokenMap[remote] = local;
         }
-        console.log('remoteToLocal:', name, remote);
+//        console.log('remoteToLocal:', name, remote);
         return local;
     };
     var proxy = function proxy(remote) {
-        console.log('proxy:', name, remote);
+//        console.log('proxy:', name, remote);
         return sponsor(function (message) {
             remoteSend(remote, message);
         });
@@ -107,7 +107,7 @@ marshal.domain = function domain(name, sponsor) {
         } : undefined);
     };
     var localSend = function localSend(remote, json) {
-        console.log('localSend:', remote, json);
+//        console.log('localSend:', remote, json);
         var local = tokenMap[remote];
         if (!local) { throw Error('Unknown remote: ' + remote); }
         var message = decode(json);
